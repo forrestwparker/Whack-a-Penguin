47de6753f205e224ae297d09a0fb010212ae97ec
Forrest Parker <forrestwparker@gmail.com>
2018-05-05 14:13:50 -0700

    Completed Section 2: Getting up and running: SKCropNode

--- Whack-a-Penguin/GameViewController.swift
+++ Whack-a-Penguin/GameViewController.swift
@@ -2,9 +2,6 @@
 //  GameViewController.swift
 //  Whack-a-Penguin
 //
-//  Created by Forrest Parker on 5/5/18.
-//  Copyright © 2018 Forrest Parker. All rights reserved.
-//
 
 import UIKit
 import SpriteKit
